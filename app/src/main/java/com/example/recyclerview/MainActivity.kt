a881cc9081ee85503d1e7bf64c9a7eaae5c19850
Aleksandr Tegai <shura.tegay@mail.ru>
2022-04-28 17:13:18 +0300

    Added: - Libraries: javaFaker, recyclerView, glide
    XML: item_user.xml, activity_main.xml
    
    Model: User.kt
    App.kt
    
    UserService.kt {
    
     - init.users
     - fun deleteUser
     - fun moveUser
     - addListener
     - removeListener
     - notifyChanges
    
    }
    
    UsersAdapter.kt {
    
    - fun onClick
    - fun showPopupMenu
    
    }
    
    MainActivity.kt {
    
    - UsersAdapter Actions:
    -- fun onUserMove
    -- fun onUserDelete
    -- fun onUserDetails
    
    }

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.ActivityMainBinding
+import com.example.recyclerview.model.User
+import com.example.recyclerview.model.UserListener
+import com.example.recyclerview.model.UserService
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: UsersAdapter
+
+    private val userService: UserService
+        get() = (applicationContext as App).userService
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = UsersAdapter(object : UserActionListener {
+            override fun onUserMove(user: User, moveBy: Int) = userService.moveUser(user, moveBy)
+
+            override fun onUserDelete(user: User) = userService.deleteUser(user)
+
+            override fun onUserDetails(user: User) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "User: ${user.name}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        userService.addListener(userListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        userService.removeListener(userListener)
+    }
+
+    private val userListener: UserListener = {
+        adapter.users = it
+    }
+
+}
